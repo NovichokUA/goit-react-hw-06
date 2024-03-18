3e6496a0565e6976ee8d69ce620501d0d151bc99
NovichokUA <zubr7333@gmail.com>
2024-03-18 17:14:31 +0200

    delete useselect in searchbox

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,13 +1,11 @@
 import { useId } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { changeFilter } from "../../redux/filtersSlice";
 
 import css from "./SearchBox.module.css";
 import { FaSistrix } from "react-icons/fa6";
 
 export const SearchBox = () => {
-  const selectNameFilter = useSelector((state) => state.filters.name);
-
   const dispatch = useDispatch();
   const id = useId();
   return (
@@ -18,7 +16,6 @@ export const SearchBox = () => {
         className={css.input}
         id={id}
         type="text"
-        value={selectNameFilter}
         onChange={(e) => {
           dispatch(changeFilter(e.target.value));
         }}

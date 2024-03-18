744cf52c1d5114c3db99d80a451e3ebc1ca5c248
NovichokUA <zubr7333@gmail.com>
2024-03-18 09:18:34 +0200

    add code

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
-import "./App.css";
+import css from "../src/App.module.css";
+import { ContactForm } from "./components/ContactForm/ContactForm";
+import ContactList from "./components/ContactList/ContactList";
+import { SearchBox } from "./components/SearchBox/SearchBox";
 
-function App() {
-  return <></>;
+export function App() {
+  return (
+    <div className={css.mainContainer}>
+      <h1 className={css.title}>Phonebook</h1>
+      <ContactForm />
+      <SearchBox />
+      <ContactList />
+    </div>
+  );
 }
-
-export default App;
